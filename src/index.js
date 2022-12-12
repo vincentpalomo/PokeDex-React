25cab50e6f0d5d6a49a551d6fc01716847648097
vincent <vincentpalomo@yahoo.com>
2022-12-11 22:29:19 -0600

    fixed background to fit screen

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import styled from 'styled-components';
 import PokeDex from './components/PokeDex';
 
 const Home = () => {
   return (
-    <div className='root'>
+    <Root className='root'>
       <PokeDex />
-    </div>
+    </Root>
   );
 };
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(<Home />);
+
+const Root = styled.div`
+  background-color: cornflowerblue;
+`;
